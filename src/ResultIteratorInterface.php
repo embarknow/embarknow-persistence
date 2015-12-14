6d758d3d653da6d9ec7163e11d13cc7647d07790
John Porter <john@designermonkey.co.uk>
2015-12-14 22:27:02 +0000

    Remove non required method

--- src/ResultIteratorInterface.php
+++ src/ResultIteratorInterface.php
@@ -10,11 +10,5 @@ use Countable;
  */
 interface ResultIteratorInterface extends Iterator, Countable
 {
-    /**
-     * Process each record into a callback function
-     * @param  callable $callback
-     * @param  array    $arguments
-     * @return void
-     */
-    public function each(callable $callback, array $arguments = []);
+
 }
